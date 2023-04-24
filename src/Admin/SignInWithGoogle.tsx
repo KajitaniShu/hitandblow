df7ee90aa0e78169998967508015d9808b1d0e73
shu kajitani <hiiragi.univ@gmail.com>
2023-04-24 21:32:21 +0900

    Fix スマホだとGoogleログインが機能しない問題を解消

--- src/Admin/SignInWithGoogle.tsx
+++ src/Admin/SignInWithGoogle.tsx
@@ -1,16 +1,23 @@
 import React from 'react'
-import { signInWithPopup } from 'firebase/auth'
 import { auth } from '../Config/firebase'
 import { provider } from '../Config/firebase';
 import { FcGoogle } from 'react-icons/fc';
 import { Button } from '@mantine/core';
-import { getAuth, signInWithRedirect } from "firebase/auth";
+import { getAuth, signInWithPopup, signInWithRedirect } from "firebase/auth";
+import { useViewportSize  } from '@mantine/hooks';
 
-function hundleSignIn() {
-  signInWithRedirect(auth, provider);
-}
+
 
 export default function SignInWithGoogle() {
+  const { width, height } = useViewportSize();
+  function hundleSignIn() {
+    if(width > 1000){
+      signInWithRedirect(auth, provider);
+    }else{
+      signInWithPopup(auth, provider);
+    }
+  }
+
   return (
     <Button leftIcon={<FcGoogle />} onClick={hundleSignIn} variant="default" color="gray">Googleでログイン</Button>
   )
